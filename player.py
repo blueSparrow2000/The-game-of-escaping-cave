ff04a4aaadcf6e789e4a24b954b8774d58d6e367
blueSparrow2000 <86525009+blueSparrow2000@users.noreply.github.com>
2021-08-15 20:35:07 +0900

    Created new statuses module for organization

--- player.py
+++ player.py
@@ -1,121 +1,16 @@
 import random, math
-import items, world, util, title
-
+import items, world, util, title, statuses
 '''
 For player's actions, you also need to define wrapper classes (to use them with hotkeys) in the actions.py module.
 All the effects of player's actions must be written here.
 '''
-class Status:
-    def __init__(self,name,description, level):
-        self.name = name
-        self.description = description
-        self.level = level
-        self.max = 10  # 모든 status들은 다 10이 최대치이다
-
-    def __str__(self):  # level up시 참고 정보로 출력할것임.
-        return "{}\n=====\n{}\nLevel: {}\n[Ability] {}".format(self.name, self.description, self.level, self.get_ability())
-
-    def get_ability(self):
-        return ''
-
-    def upgradable(self):
-        return self.level == self.max
-
-    def upgrade(self):
-        self.level += 1
-        print('\nUpgraded {} by 1 levels.\n{} level: {}'.format(self.name,self.name,self.level))
-
-class Agility(Status):
-    def __init__(self):
-        super().__init__(name='Agility',
-                         description='Increase flee rate, increase attack evasion(dodging).', level = 0)
-
-    def flee_prob(self):
-        return self.level/10
-
-    def dodge_prob(self):
-        return self.level/20
-
-    def get_ability(self):
-        return '\nFlee probability: {} %\nDodge probability: {} %\n'.format(int(self.flee_prob()*100),int(self.dodge_prob()*100))
-
-class Defence(Status):
-    def __init__(self):
-        super().__init__(name='Defence',
-                         description='Decreases amount of damage taken.', level = 0)
-
-    def damage_decrease_multiplier(self):
-        return (1-round(0.8*math.log10(self.level+1),2))
-
-    def get_ability(self):
-        return '\nDamage decrease rate: {} %\n'.format(int((1-self.damage_decrease_multiplier())*100))
-
-class Learning(Status):
-    def __init__(self):
-        super().__init__(name='Learning',
-                         description='Increase xp gain rate.', level = 0)
-
-    def learning_multiplier(self):
-        return (1+self.level/10)
-
-    def get_ability(self):
-        return '\nLearning rate increase: {} %\n'.format(int((self.learning_multiplier()-1)*100))
-
-class Strength(Status):
-    def __init__(self):
-        super().__init__(name='Strength (Physical)',
-                         description='Increase physical weapon damage - weapon that does not use magic.', level = 0)
-
-    def strength_multiplier(self,weapon):
-        if not isinstance(weapon, items.Magical):
-            return self.formula()
-        return 1
-
-    def formula(self):
-        return (1 + self.level / 10)
-
-    def get_ability(self):
-        return '\nStrength increase: {} %\n'.format(int((self.formula()-1)*100))
-
-
-class MagicAffinity(Status):
-    def __init__(self):
-        super().__init__(name='Magic Affinity',
-                         description='Increase magic damage when using Wand type weapons.', level = 0)
-
-    def magic_multiplier(self,weapon):
-        if isinstance(weapon, items.Magical):
-            return self.formula()
-        return 1
-
-    def formula(self):
-        return math.ceil(10 + random.uniform(2.5*self.level,3*self.level))/10
-
-    def get_ability(self):
-        return '\nMagic damage increase: {} ~ {} %\n'.format(int(2.5*(self.level/10)*100),int(3*(self.level/10)*100))
-
-
-class Stability(Status):
-    def __init__(self):
-        super().__init__(name='Stability',
-                         description="Reduces the deviation of the weapon's damage through your concentration.", level = 0)
-
-    def get_stability(self):
-        return self.formula()
-
-    def formula(self):
-        return 1-round(0.8*math.log10(self.level+1),2)
-
-    def get_ability(self):
-        return '\nDeviation reduce rate: {} %\n'.format(int((1-self.formula())*100))
-
 
 class Player:
     def __init__(self,name):
         self.name = name
         self.title = ''
         self.inventory = [items.Gold(25), items.Rock(), items.Apple(),items.Bow(),items.Arrow(),items.Wand()]
-        self.stats = {'agi': Agility(), 'def': Defence(), 'lrn': Learning(), 'str': Strength(), 'mga': MagicAffinity(), 'stb': Stability()}
+        self.stats = {'agi': statuses.Agility(), 'def': statuses.Defence(), 'lrn': statuses.Learning(), 'str': statuses.Strength(), 'mga': statuses.MagicAffinity(), 'stb': statuses.Stability()}
         self.hp = 100
         self.hpmax = 100
         self.level = 0
@@ -467,7 +362,8 @@ class Player:
         if selected_weapon: # if not quit (item is returned)
             print("You use {} against {}!".format(selected_weapon.name, enemy.name))
             dmg = selected_weapon.get_damage(self.stats['stb'].get_stability(),self.stats['str'].strength_multiplier(selected_weapon),self.stats['mga'].magic_multiplier(selected_weapon))
-            enemy.hp -= math.ceil(dmg)
+            enemy.hp -= round(dmg,1)
+            print("It did {} damage!".format(round(dmg,1)))
 
             if isinstance(selected_weapon, items.Shootable):  # consume ammo
                 self.consume_ammo(selected_weapon)
