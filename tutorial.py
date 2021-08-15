d7689052d0a94016ded7211fca88ae883222560c
blueSparrow2000 <86525009+blueSparrow2000@users.noreply.github.com>
2021-08-16 00:55:34 +0900

    Tutorial intro fix

--- tutorial.py
+++ tutorial.py
@@ -37,6 +37,7 @@ class Tutorial():
         self.language = None
         self.tutorial_tiles_intro_dic = {}
         self.comment = {'Korean':'좋아요!','English':'Good!'}
+        self.starting_message = {'Korean':'게임을 시작합니다!\n','English':"Let's start the game!\n"}
         self.intro_texts = {'Korean':'''
         동굴에 제 발로 들어오신 것을 환영합니다!
         여긴 튜토리얼 동굴입니다. 
@@ -56,11 +57,23 @@ class Tutorial():
         
         *: 나의 현재 위치
         ?: 내가 방문하지 않은 타일
-        S,F...: 방 이름의 앞 글자를 딴 이니셜. 서로 같은 이니셜이지만, 실제로는 다른 방일 수도 있다.
+        Sta,Gol,Fin...: 방 이름의 앞 글자를 딴 이니셜. 서로 같은 이니셜이지만, 실제로는 다른 방일 수도 있다.
         미니맵은 어디까지나 탐험을 도와주는 보조 기능일 뿐, 당신의 실력으로 탈출해야 한다!
-        
-        아래의 미니맵을 참고하십시오.
 
+        === 스테이터스(스텟) ===
+        이 게임에는 플레이어의 능력치를 증가시켜주는 스테이터스가 있습니다.
+        플레이어가 레벨업 하면 스테이터스를 1씩 올릴 수 있습니다. 
+        다만 한번 올리면 번복이 불가하니 신중하게 선택하십시오. 
+        레벨이 높을수록 레벨업에 필요한 경험치량이 (∝log) 증가합니다.
+        현재(V1.1) 민첩, 방어, 학습, 물리 공격, 마법 공격, 공격 안정성의 6가지가 존재합니다.
+        튜토리얼을 하기 전에 플레이어의 기본 레벨을 결정할때 미리 레벨업 해둘 수 있습니다.
+        각종 스텟에 대한 더 자세한 설명을 알고 싶다면 선택지의 'vs': View status 를 참고하여 'vs'를 입력하십시오.
+                
+        ======= 도감 ========   
+        지금까지 만났던 몬스터들을 적어둔 도감입니다. 
+        해당 몬스터의 체력과 공격력을 대강 알 수 있습니다. 
+        다만, 같은 이름의 몬스터라도 정보가 조금씩 다를 수 있으니 주의하십시오.
+                
         ======= 힐 ======== 
         치료하는 방법은 음식을 먹는 것입니다. 
         당신의 인벤토리에는 사과가 있으니, 나중에 그걸 드셔보십시오.
@@ -71,11 +84,11 @@ class Tutorial():
         몬스터가 있는 타일로 이동하면 몬스터가 나타납니다.
         선택지에 'z': attack 가 생기며, 'z'를 입력한 후 사용자가 원하는 무기를 선택지에서 선택하면 됩니다.
         무기의 종류와 정보는 인벤토리 'e'를 통해 전투 중을 제외하고 언제든지 볼 수 있습니다.
-        
+                
         ======= 타일(방) ========
         마지막으로 타일입니다. 
         이 게임은 플레이어가 타일을 이동하며 사건이 발생합니다.
-        흔한 타일로는 '그냥 동굴길', '금화의 방', '몬스터의 방',  '아이템의 방', '잠긴 방' 등이 있습니다.
+        흔한 타일로는 '빈(비어있는) 동굴길', '금화의 방', '몬스터의 방',  '아이템의 방', '잠긴 방' 등이 있습니다.
         동굴길은 자신의 독백을 들으실 수 있습니다.
         금화의 방엔 소량의 금화가 있습니다.
         몬스터의 방엔 몬스터가 숨어 있어서, 당신을 공격할 것입니다. 현명하게 대처하십시오.
@@ -87,6 +100,8 @@ class Tutorial():
         작은 선물을 당신의 주머니에 넣어두었습니다. 
         'e'를 입력하여 인벤토리에서 확인하십시오. 
         행운을 빕니다!
+        
+        아래는 미니맵입니다.
         ''',
 
         'English':'''
@@ -102,19 +117,31 @@ class Tutorial():
         Common actions are; basic movements (front, back, right, left) and inventory check, status check, cave monster encyclopedia view, food, etc.
         Inventory size is currently unlimited, so you don't have to worry about overflowing items.
         
-        ======= Minimap ========
+        ====== Minimap =======
         The minimap is updated every time you move.
         See the legend below.
         
         *: My current location
         ?: Tiles I haven't visited
-        S,F...: Initials from the first letter of the room name. They may have the same initials, but they may actually be different rooms.
+        Sta,Gol,Fin...: Initials from the first letter of the room name. They may have the same initials, but they may actually be different rooms.
         The mini-map is only an auxiliary function to help you explore, so you have to escape with your own skills!
-        (Do not rely on a minimap)
+        (Do not rely on a minimap!)
         
-        Please refer to the minimap below.
+        ==== Status (Stat) ====
+        In this game, there are statuses that enhance the player's ability.
+        As player level up, (s)he chooses which one of the statuses will be increased by 1.
+        Once chosen, it cannot be reversed so please choose carefully.
+        The higher the level, the greater the amount of experience required to level up (∝log).
+        Currently (V1.1), there are 6 types of stats: Agility, Defense, Learning, Strength (Physical), Magic Affinity, and Stability.
+        You can level up in advance on the beggining of the game (you probably have seen the "Enter player's level" option before this tutorial).
+        For more detailed explanations of various stats, refer to ['vs': View status] in the option (type 'vs').
+                
+        ===== Encyclopedia =====
+        This is an list containing the monsters the player have encountered so far.
+        You can roughly see the HP and damage of the monster.
+        However, be aware that even monsters with the same name may have slightly different information (HP, damage).
 
-        ======= Heal =========
+        ======== Heal ==========
         The way to heal yourself is to eat food.
         You have an apple in your inventory, try them later.
         You can eat on any tile, but if you eat during battle, you cannot attack monsters during that turn.
@@ -141,6 +168,8 @@ class Tutorial():
         This is my little present. 
         Check it in your inventory by typing 'e'. 
         Good luck!
+        
+        Below is a minimap.
         '''}
 
 
@@ -163,6 +192,7 @@ class Tutorial():
         question = {'Korean':"아무거나 눌러 튜토리얼을 종료하십시오: ",'English':"Type anything to continue: "}
         user_input = input(question[self.language])
         print(self.comment[self.language])
+        print(self.starting_message[self.language])
     #
     # def get_action(self,action):
     #     pass
