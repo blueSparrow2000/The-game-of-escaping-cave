dc63bfca108f078db5c1b4109422f2575e472d9b
blueSparrow2000 <86525009+blueSparrow2000@users.noreply.github.com>
2021-08-16 16:36:36 +0900

    Bug fix

--- items.py
+++ items.py
@@ -16,20 +16,20 @@ class Item():
 class RabbitFoot(Item):
     def __init__(self, resurrection_hp):
         self.resurrection_hp = resurrection_hp
-        super().__init__(name='Rabbit foot',
-                         description='It feels like something lucky is gonna happen!',
-                         value=1000)
+        super().__init__(name='\033[46mRabbit foot\033[0m',
+                         description="It feels like something lucky☆ is gonna happen!",
+                         value=100+resurrection_hp)
 
 class Gold(Item):
     def __init__(self, amt):
         self.amt = amt
-        super().__init__(name='Gold', description='A round coin with {} stamped on the front!'.format(str(self.amt)),
+        super().__init__(name='\033[103mGold\033[0m', description='A round coin with {} stamped on the front!'.format(str(self.amt)),
                          value=self.amt)
 
 class Key(Item):
     def __init__(self, address_code):
         self.address_code = address_code
-        super().__init__(name='Key', description='A key that can open a door somewhere. {} stamped on the back.'.format(self.address_code), value=100)
+        super().__init__(name='\033[90m{}\033[0m'.format('Key'), description='A key that can open a door somewhere. {} stamped on the back.'.format(self.address_code), value=100)
 
     def can_open(self,locked_state):
         return self.address_code == locked_state
@@ -62,7 +62,7 @@ class Food(Item):
         return True
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\nHeal amount: {}".format(self.name, self.description,
+        return "{}\n=====\n{}\nValue: {}\nHeal amount: \033[92m{}\033[0m".format(self.name, self.description,
                                                                                  self.value, self.healamt)
 
 class XpOrb(Food):
@@ -79,15 +79,15 @@ class XpOrb(Food):
 
 class Apple(Food):
     def __init__(self):
-        super().__init__(name='Apple', description='Pale green apple. Looks tasty.', value=5, healamt=10)
+        super().__init__(name='Apple', description='Pale green apple. Looks tasty.', value=10, healamt=10)
 
 class BeefJerky(Food):
     def __init__(self):
-        super().__init__(name='BeefJerky', description='Slices of well-dried meat. It smells like cow hide...', value=20, healamt=35)
+        super().__init__(name='BeefJerky', description='Slices of well-dried meat. It smells like cow hide...', value=20, healamt=25)
 
 class pill(Food):
     def __init__(self):
-        super().__init__(name='Pill', description='One pill will heal everything.', value=100, healamt=100)
+        super().__init__(name='Pill', description='One pill will heal everything.', value=80, healamt=100)
 
 ################################################# Weapon
 # damage가 높을수록 damage_deviation가 낮아지는 경향이 있음. 마법 무기는 그게 극심함.
@@ -106,17 +106,21 @@ class Weapon(Item):
         super().__init__(name, description, value)
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\nDamage: {} ~ {}\nRequired level: {}".format(self.name, self.description,
-                                                                                 self.value, self.damage - self.damage_deviation,self.damage + self.damage_deviation,
+        return "{}\n=====\n{}\nValue: {}\nDamage: \033[91m{}\033[0m ~ \033[91m{}\033[0m   (avg: \033[91m{}\033[0m / dev: \033[91m{}\033[0m)\nRequired level: {}".format(self.name, self.description,
+                                                                                 self.value, round(self.damage - self.damage_deviation,1),round(self.damage + self.damage_deviation,1),
+                                                                                                        round(self.damage,1),round(self.damage_deviation,1),
                                                                                  self.lvrestriction)
     def is_ammo(self,item):
         if self.ammoname:
             return self.ammoname == item.name
 
     def get_damage(self,stability,str_mul,mga_mul):
-        deviation = self.damage_deviation*(stability) # 안정성 % 비율만큼 deviation이 줄어든다
+        deviation = self.damage_dev_considering_stability(stability) # 안정성 % 비율만큼 deviation이 줄어든다
         damage = self.damage*str_mul*mga_mul + random.uniform(-deviation,deviation)
         return round(damage,1) # 소수점 첫째자리에서 반올림
+
+    def damage_dev_considering_stability(self,stability):
+        return self.damage_deviation * stability  # 안정성 % 비율만큼 deviation이 줄어든다
 
 
 class Rock(Weapon):
@@ -133,7 +137,7 @@ class Dagger(Weapon):
 class Shield(Weapon):
     def __init__(self):
         luck = math.ceil(random.uniform(0, 5))
-        super().__init__(name='Shield', description="Have you ever heard of a shield warrior? If in inventory, reduces 20% of the damage.",
+        super().__init__(name='\033[36mShield\033[0m', description="Have you ever heard of a shield warrior? If in inventory, reduces 20% of the damage.",
                          value=50 + luck*2, damage=(5 + luck), damage_deviation=2,ammoname = None,lvrestriction=1)
 
 
@@ -147,7 +151,7 @@ class Magical(Weapon):  # Weapon that is magical
         self.damage_deviation = round(3*self.damage_deviation//4,1)
 
     def dull(self): # increases damage dev
-        damage_deviation = round(self.damage_deviation*1.5,1)
+        damage_deviation = round(self.damage_deviation*1.2,1)
         lowest_damage = self.damage - damage_deviation
         if lowest_damage <0:
             damage_deviation = self.damage-1 # slightly less than avg damage (so that lowest damage is 1)
@@ -156,11 +160,11 @@ class Magical(Weapon):  # Weapon that is magical
     def strange(self):  # strange...
         self.damage += 5
 
-    def charming(self): # In progress... (currently nothing)
+    def charming(self): # In progress... (currently does nothing)
         return
 
-### global variables for Magical
-type_dic = {'『sharp』': Magical.sharp, '『dull』':Magical.dull, '『strange』':Magical.strange, '『charming』':Magical.charming}
+### global variables for Magical & helper functions
+type_dic = {'\033[90m{}\033[0m'.format('『sharp』'): Magical.sharp, '\033[100m\033[97m{}\033[0m'.format('『dull』'):Magical.dull, '\033[96m{}\033[0m'.format('『strange』'):Magical.strange, '\033[95m{}\033[0m'.format('『charming』'):Magical.charming}
 type_list = list(type_dic.keys())
 def type_initializer(object,type):
     global type_dic, type_list
@@ -172,23 +176,25 @@ def choose_type():
     global type_dic, type_list
     return random.choice(type_list)
 
+##################################################
+
 class Wand(Magical):
     def __init__(self):
         self.type = choose_type()
-        super().__init__(name='Basic Wand', description="A long stick that helps casting spells. Don't expect too much.\nThis wand is a {} type.".format(self.type),
+        super().__init__(name='\033[95mBasic Wand\033[0m', description="A long stick that helps casting spells. Don't expect too much.\nThis wand is a {} type.".format(self.type),
                          value=50, damage=16 ,damage_deviation=12,ammoname = None, lvrestriction=3)
         type_initializer(self,self.type)
 
 class Staff(Magical):
     def __init__(self):
         self.type = choose_type()
-        super().__init__(name='Mage Staff', description="A staff used by licensed wizards. It is advanced type of a wand.\nThis staff is a {} type.".format(self.type),
+        super().__init__(name='\033[95mMage Staff\033[0m', description="A staff used by licensed wizards. It is advanced type of a wand.\nThis staff is a {} type.".format(self.type),
                          value=50, damage=30 ,damage_deviation=24, ammoname = None, lvrestriction=6)
         type_initializer(self,self.type)
 
 class Scepter(Magical):
     def __init__(self):
-        super().__init__(name='Scepter',
+        super().__init__(name='\033[95mScepter\033[0m',
                          description="Long stick.",
                          value=50, damage=100, damage_deviation=0, ammoname=None, lvrestriction=20)
 ################################################################################################### Shootable
