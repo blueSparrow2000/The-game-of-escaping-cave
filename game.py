83ad9ac1c80a0f8696d4d1cce2b77aa6e585f9d9
blueSparrow2000 <86525009+blueSparrow2000@users.noreply.github.com>
2021-08-17 23:04:12 +0900

    V1.2 Release - Story mode update

--- game.py
+++ game.py
@@ -7,15 +7,16 @@ import items  # to give player some good item in the tutorial
 print('''
 
         <The Cave adventure>
-                V1.1
+                V1.2
 
     = Turn based text RPG game! =
 
 
-* Features: 
-- Player can now choose weapons when attacking!
-- Tutorial is now ready! (It's just long lines of texts though... yet...)
-- Memory room update coming soon... (story mode)
+* Features: Story mode update
+
+- Try story mode! 
+    How to try story mode: 
+    When selecting maps, choose 'prison' map
 =================================================================================================================
 ''')
 
@@ -36,10 +37,15 @@ def play():
     playerminimap = world.load_tiles(map_name, mode = selected_mode, reveal = map_reveal) # Player의 starting position이 정해짐!
     player = Player(player_name,playerminimap)  #player의 starting position을 먼저 정하고 나서 player를 만들어야 한다! 주의!
 
-    if selected_mode =='Easy':
-        player.give(items.RabbitFoot(50))
+    setting.give_items_corresponding_to_mode(player, mode = selected_mode)
 
     setting.set_player_level(player)
+
+    # story mode setting!
+    story_bot = story_info.EmptyBot(player)
+    if map_name == 'prison':
+        story_bot = story_info.PrisonStoryBot(player)
+        story_bot.turn_on()
 
     print('='*70,'\n','''
     setting complete!
@@ -85,13 +91,11 @@ def play():
             for action in available_actions:
                 if action_input == action.hotkey:
                     player.do_action(action, **action.kwargs)
+
+                    story_bot.scan_player_attributes()
+
                     last_action = action
                     break
-
-            # Check again since the room could have changed the player's state & print the minimap
-            # if actions.check_movement(last_action):  # player가 움직임(started game/flee/move)을 선택했다면 minimap을 보여주도록 함!
-            #     playerminimap.update(player.location_x, player.location_y)
-            #     player.show_minimap()
 
             current_location = player.get_location()
             if last_location != current_location: # 플레이어가 실제로 움직였을때
@@ -101,16 +105,17 @@ def play():
                 standing_tile = world.tile_exists(player.location_x, player.location_y)
                 if standing_tile: # None이 리턴될 리는 없다. available action만 가능하기 때문에. 그래도 혹시나 해서 넣음. 필요없으면 제거!
                     standing_tile.intro(player) # 방에 방금 들어왔을 때만 intro를 해야함. (intro는 방에 처음 들어갔을때 실행되는 것임. modify는 방에 있으면 지속적으로 할수도 있음)
+                story_bot.update_player_visited_tiles()
+                story_bot.show_story()
 
     if not player.is_alive():
         print("\n","="*70,"\n","="*70,"\nYou died... ㅠㅠ")
     if player.victory:
-        print("\n", "=" * 70, "\nCongratulations! '{}' has escaped the {}!".format(player.name+player.title,map_name),"\n", "=" * 70)
-
+        if not story_bot.final_text():
+            print("\n", "=" * 70, "\nCongratulations! '{}' has escaped the {}!".format(player.name+player.title,map_name),"\n", "=" * 70)
 
 if __name__ == "__main__":
     while 1:
         play()
         if util.ask_player('Play again?', ['Y', 'N']) == 'N':
             break
-
